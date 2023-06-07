a3649428f29dbb901ac6a875a99e16e3b2f6c7dc
amirhamza24 <ridoykhan7897890@gmail.com>
2023-06-07 21:46:09 +0600

    signup page

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBRC6pdsVhwLi5VZYrqgJmprT_kMQiCshk",
-  authDomain: "bistro-boss-92937.firebaseapp.com",
-  projectId: "bistro-boss-92937",
-  storageBucket: "bistro-boss-92937.appspot.com",
-  messagingSenderId: "1045015000643",
-  appId: "1:1045015000643:web:d3886be005c4674c8e86f6"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase

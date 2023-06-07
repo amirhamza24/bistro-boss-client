c63b889ba2810f1455f422a6de791dd8923ee08f
amirhamza24 <ridoykhan7897890@gmail.com>
2023-06-08 00:50:44 +0600

    signup page update

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,15 +1,32 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 const Navbar = () => {
+
+    const { user, logOut } = useContext(AuthContext)
+
+    const handleLogOut = () => {
+        logOut()
+        .then(() => {})
+        .catch(error => console.log(error))
+    }
+
     const navOptions = (
         <>
             <li><Link to='/'>Home</Link></li>
-
             <li><Link to='/menu'>Our Menu</Link></li>
-
             <li><Link to="/order/salad">Order Food</Link></li>
+            <li><Link to='/secret'>Secret</Link></li>
 
-            <li><Link to='/login'>Login</Link></li>
+            {
+                user ? <>
+                    <span>{user?.displayName}</span>
+                    <button onClick={handleLogOut} className="btn btn-ghost">Log out</button>
+                </> : <>
+                    <li><Link to='/login'>Login</Link></li>
+                </>
+            }
 
 
         </>
@@ -49,7 +66,7 @@ const Navbar = () => {
                     <ul className="menu menu-horizontal px-1">{navOptions}</ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <a className="btn">Get Started</a>
                 </div>
             </div>
         </div>
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth"
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth"
 import { app } from "../firebase/firebase.config";
 
 
@@ -18,12 +18,18 @@ const AuthProvider = ({ children }) => {
 
     const signIn = (email, password) => {
         setLoading(true);
-        return signInWithEmailAndPassword(email, password);
+        return signInWithEmailAndPassword(auth, email, password);
     }
 
     const logOut = () => {
         setLoading(false);
         return signOut(auth);
+    }
+
+    const updateUserProfile = (name, photo) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photo
+        });
     }
 
     useEffect( () => {
@@ -44,6 +50,7 @@ const AuthProvider = ({ children }) => {
         createUser,
         signIn,
         logOut,
+        updateUserProfile,
     }
 
     return (

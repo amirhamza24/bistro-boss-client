823ba0d99f9a9f1240cc5afa9abc5d0f90706706
amirhamza24 <ridoykhan7897890@gmail.com>
2023-06-08 19:26:11 +0600

    dashboard menu itemt added

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
             <li><Link to="/order/salad">Order Food</Link></li>
             <li><Link to='/secret'>Secret</Link></li>
             <li>
-                <Link to="/">
+                <Link to="/dashboard/mycart">
                     <button className="btn btn-gap-2">
                         <FaShoppingCart></FaShoppingCart>
                         <div className="badge badge-secondary">+{cart?.length || 0}</div>
